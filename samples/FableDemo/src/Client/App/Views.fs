4886f6b287ee4ec1ef271390b42b1bf9a4bb347e
binwwu <binwwu@ebay.com>
2019-09-19 14:00:31 +0800

    Use Union type as parameters

--- samples/FableDemo/src/Client/App/Views.fs
+++ samples/FableDemo/src/Client/App/Views.fs
@@ -21,18 +21,20 @@ let icon cs = span [ Classes cs ] []
 
 module Buttons =
   let button classes label onClick =
-      button [
-        OnClick onClick
-        Classes [
-          yield tailwind.``m-02``
-          yield tailwind.``py-01``
-          yield tailwind.``px-02``
-          yield tailwind.rounded
-          yield! classes
+      button
+        [
+          OnClick onClick
+          Classes [
+            yield tailwind.``m-02``
+            yield tailwind.``py-01``
+            yield tailwind.``px-02``
+            yield tailwind.rounded
+            yield! classes
+          ]
         ]
-      ] [
-        str label
-      ]
+        [
+          str label
+        ]
 
   let primaryButton =
       button [
@@ -49,8 +51,8 @@ module Buttons =
       ]
 
 
-type MyLightForm() =
-  static member formOuterClasses =
+module MyLightForm =
+  let formOuterClasses =
     classes [
       tailwind.border
       tailwind.``border-gray-200``
@@ -60,209 +62,220 @@ type MyLightForm() =
       tailwind.rounded
     ]
 
-  static member formLabelClasses =
+  let formLabelClasses =
     classes [
       tailwind.``text-gray-600``
       tailwind.``text-sm``
       tailwind.``pb-01``
     ]
 
-  static member formErrorClasses =
+  let formErrorClasses =
     classes [
       tailwind.``text-red-400``
       tailwind.``mt-01``
       tailwind.``text-sm``
     ]
 
-  static member input(label, ty, ?leftIconClasses, ?rightIconClasses, ?inputClasses) =
+  type MyInputProp =
+    | InputProps of InputProp list
+    | LeftIconClasses of string list
+    | RightIconClasses of string list
+
+  let input props =
+    let leftIconClasses  = props |> UnionCase.caseValues (MyInputProp.LeftIconClasses []) |> UnionCase.values |> List.concat
+    let rightIconClasses = props |> UnionCase.caseValues (MyInputProp.RightIconClasses []) |> UnionCase.values |> List.concat
+
     let iconView iconClasses =
-      icon
+      icon [
+        yield tailwind.block
+        yield tailwind.``bg-gray-300``
+        yield tailwind.``py-02``
+        yield tailwind.``px-02``
+        yield tailwind.``text-center``
+        yield! iconClasses
+      ]
+
+    let inputWrapper v =
+      div
         [
-          yield tailwind.block
-          yield tailwind.``bg-gray-300``
-          yield tailwind.``py-02``
-          yield tailwind.``px-02``
-          yield tailwind.``text-center``
-          yield! iconClasses
+          Classes [ tailwind.flex ]
+        ]
+        [
+          if List.isEmpty leftIconClasses |> not then
+            yield iconView [ yield tailwind.``rounded-l``; yield! leftIconClasses ]
+
+          yield v
+
+          if List.isEmpty rightIconClasses |> not then
+            yield iconView [ yield tailwind.``rounded-r``; yield! rightIconClasses ]
         ]
 
-    Fields.input
-      (ty
-      ,label = label
-      ,outerClasses = MyLightForm.formOuterClasses
-      ,inputClasses =
-          classes [
-            yield tailwind.``outline-none``
-            yield tailwind.``bg-gray-200``
-            yield tailwind.``py-01``
-            yield tailwind.``px-03``
-            yield tailwind.``w-full``
-            yield tailwind.``focus:border-blue-400``
-            yield tailwind.``focus:bg-blue-100``
-            yield tailwind.``hover:bg-blue-200``
-            yield tailwind.``text-gray-700``
-            yield tailwind.``rounded-none``
-            if leftIconClasses.IsNone then
-              yield tailwind.``rounded-l``
-            if rightIconClasses.IsNone then
-              yield tailwind.``rounded-r``
-            if inputClasses.IsSome then
-              yield! inputClasses.Value
-          ]
-      ,labelClasses = MyLightForm.formLabelClasses
-      ,errorClasses = MyLightForm.formErrorClasses
-      ,inputViewWrapper = fun v ->
-          div
-            [
-              Classes
-                [
-                  tailwind.flex
-                ]
-            ]
-            [
-              if leftIconClasses.IsSome then
-                yield iconView [ yield tailwind.``rounded-l``; yield! leftIconClasses.Value ]
-
-              yield v
-
-              if rightIconClasses.IsSome then
-                yield iconView [ yield tailwind.``rounded-r``; yield! rightIconClasses.Value ]
-            ])
-
-
-  static member selects(label, sourceList, displayer) =
-    Fields.selector
-      (sourceList
-      ,label = label
-      ,displayer = displayer
-      ,outerClasses = MyLightForm.formOuterClasses
-      ,inputClasses =
-          classes [
-            tailwind.flex
-            tailwind.``items-center``
-            tailwind.``text-gray-700``
-            tailwind.``ml-01``
-          ]
-      ,labelClasses = MyLightForm.formLabelClasses
-      ,errorClasses = MyLightForm.formErrorClasses)
-
-
-  static member errorSummary form =
-    div [
-      Classes [
-        tailwind.``text-red-500``
-        tailwind.``text-sm``
-        tailwind.``text-center``
-        tailwind.``p-02``
-      ]
-    ] [
-      for e in getFormErrors form -> div [] [ sprintf "* %s" e |> str ]
+    inputField [
+      yield InputProp.OuterClasses formOuterClasses
+      yield InputProp.InputClasses (classes [
+        yield tailwind.``outline-none``
+        yield tailwind.``bg-gray-200``
+        yield tailwind.``py-01``
+        yield tailwind.``px-03``
+        yield tailwind.``w-full``
+        yield tailwind.``focus:border-blue-400``
+        yield tailwind.``focus:bg-blue-100``
+        yield tailwind.``hover:bg-blue-200``
+        yield tailwind.``text-gray-700``
+        yield tailwind.``rounded-none``
+        if List.isEmpty leftIconClasses then
+          yield tailwind.``rounded-l``
+        if List.isEmpty rightIconClasses then
+          yield tailwind.``rounded-r``
+      ])
+      yield InputProp.LabelClasses formLabelClasses
+      yield InputProp.ErrorClasses formErrorClasses
+      yield InputProp.InputViewWrapper inputWrapper
+      yield! (props |> UnionCase.caseValues (MyInputProp.InputProps []) |> UnionCase.values |> List.concat)
     ]
 
 
+  let inline selector props =
+    selectorField [
+      yield SelectorProp.OuterClasses formOuterClasses
+      yield SelectorProp.InputClasses (classes [
+        tailwind.flex
+        tailwind.``items-center``
+        tailwind.``text-gray-700``
+        tailwind.``ml-01``
+      ])
+      yield SelectorProp.LabelClasses formLabelClasses
+      yield SelectorProp.ErrorClasses formErrorClasses
+      yield! props
+    ]
+
+  let errorSummary form =
+    div
+      [
+        Classes [
+          tailwind.``text-red-500``
+          tailwind.``text-sm``
+          tailwind.``text-center``
+          tailwind.``p-02``
+        ]
+      ]
+      [
+        for e in getFormErrors form -> div [] [ sprintf "* %s" e |> str ]
+      ]
+
+
+open MyLightForm
+
 let app state dispatch =
-    let field = Fields.field state.UserProfileForm (UserProfileMsg >> dispatch)
+    let field key renderer arg = field state.UserProfileForm (UserProfileMsg >> dispatch) key (renderer arg)
 
     div
       [
-        Classes
-          [
-            tailwind.``font-sans``
-            tailwind.``h-screen``
-            tailwind.flex
-            tailwind.``flex-wrap``
-            tailwind.``justify-center``
-            tailwind.``items-center``
-          ]
+        Classes [
+          tailwind.``font-sans``
+          tailwind.``h-screen``
+          tailwind.flex
+          tailwind.``flex-wrap``
+          tailwind.``justify-center``
+          tailwind.``items-center``
+        ]
       ]
       [
         form
           [
-            Classes
-              [
-                tailwind.``sm:w-full``
-                tailwind.``md:w-01/04``
-                tailwind.``shadow-lg``
-              ]
+            Classes [
+              tailwind.``sm:w-full``
+              tailwind.``md:w-01/04``
+              tailwind.``shadow-lg``
+            ]
           ]
           [
-            field "UserName"
-              (MyLightForm.input
-                ("Email"
-                ,InputType.Email
-                ,leftIconClasses = [ fontAwsome.fa; fontAwsome.``fa-mail-bulk`` ]))
+            field "UserName" input [
+              MyInputProp.InputProps [
+                InputProp.Label "Email"
+                InputProp.InputType InputType.Email
+              ]
+              MyInputProp.LeftIconClasses [ fontAwsome.fa; fontAwsome.``fa-mail-bulk`` ]
+            ]
 
-            field "Password"
-              (MyLightForm.input
-                ("Password"
-                ,InputType.Password
-                ,leftIconClasses = [ fontAwsome.fa; fontAwsome.``fa-lock``; tailwind.``text-green-500`` ]
-                ,inputClasses = [ tailwind.``text-green-400`` ]))
+            field "Password" input [
+              MyInputProp.InputProps [
+                InputProp.Label "Password"
+                InputProp.InputType InputType.Password
+              ]
+              MyInputProp.LeftIconClasses [ fontAwsome.fa; fontAwsome.``fa-lock``; tailwind.``text-green-500`` ]
+              MyInputProp.RightIconClasses [ tailwind.``text-green-400`` ]
+            ]
 
-            field "Birthday"
-              (MyLightForm.input
-                ("Birthday"
-                ,InputType.Date "yyyy/MM/dd"
-                ,inputClasses = [ tailwind.``text-purple-500`` ]))
+            field "Birthday" input [
+              MyInputProp.InputProps [
+                InputProp.Label "Birthday"
+                InputProp.InputType (InputType.Date "yyyy/MM/dd")
+                InputProp.InputClasses (classes [ tailwind.``text-purple-500`` ])
+              ]
+            ]
 
-            field "Roles"
-              (MyLightForm.selects
-                ("Roles"
-                ,[ 1, "R1"; 2, "R2" ]
-                ,fun (_,v) -> span [ Classes [ tailwind.``ml-01`` ] ] [ str v ]))
+            field "Roles" selector [
+              SelectorProp.Label "Roles"
+              SelectorProp.SourceList [ 1, "R1"; 2, "R2" ]
+              SelectorProp.Displayer (fun (_,v) -> span [ Classes [ tailwind.``ml-01`` ] ] [ str v ])
+            ]
 
-            field "Address:Country"
-              (MyLightForm.input
-                ("Country"
-                ,InputType.Text
-                ,rightIconClasses = [ fontAwsome.fa; fontAwsome.``fa-map-marked`` ]))
+            field "Address:Country" input [
+              MyInputProp.InputProps [
+                InputProp.Label "Country"
+                InputProp.InputType InputType.Text
+              ]
+              MyInputProp.RightIconClasses [ fontAwsome.fa; fontAwsome.``fa-map-marked`` ]
+            ]
 
-            field "Address:Street"
-              (MyLightForm.input
-                ("Street"
-                ,InputType.Text
-                ,leftIconClasses = [ fontAwsome.fa; fontAwsome.``fa-funnel-dollar``; tailwind.``text-orange-400`` ]
-                ,rightIconClasses = [ fontAwsome.fa; fontAwsome.``fa-passport``; tailwind.``text-red-500`` ]))
+            field "Address:Street" input [
+              MyInputProp.InputProps [
+                InputProp.Label "Street"
+                InputProp.InputType InputType.Text
+              ]
+              MyInputProp.LeftIconClasses [ fontAwsome.fa; fontAwsome.``fa-funnel-dollar``; tailwind.``text-orange-400`` ]
+              MyInputProp.RightIconClasses [ fontAwsome.fa; fontAwsome.``fa-passport``; tailwind.``text-red-500`` ]
+            ]
 
-            field "Address:ZipCode"
-              (MyLightForm.input
-                ("ZipCode"
-                ,InputType.Number))
+            field "Address:ZipCode" input [
+              MyInputProp.InputProps [
+                InputProp.Label "ZipCode"
+                InputProp.InputType InputType.Number
+              ]
+            ]
 
-            MyLightForm.errorSummary state.UserProfileForm
+            errorSummary state.UserProfileForm
 
             div
               [
-                Classes
-                  [
-                    tailwind.``text-center``
-                    tailwind.``mb-02``
-                  ]
+                Classes [
+                  tailwind.``text-center``
+                  tailwind.``mb-02``
+                ]
               ]
               [
                 Buttons.primaryButton  "Submit" ignore
                 Buttons.secondayButton "Cancel" ignore
               ]
-            icon
-              [
-                fontAwsome.fab
-                fontAwsome.``fa-github``
-                tailwind.block
-                tailwind.``bg-blue-200``
-                tailwind.``py-02``
-                tailwind.``text-center``
-                tailwind.``text-green-800``
-              ]
+            icon [
+              fontAwsome.fab
+              fontAwsome.``fa-github``
+              tailwind.block
+              tailwind.``bg-blue-200``
+              tailwind.``py-02``
+              tailwind.``text-center``
+              tailwind.``text-green-800``
+            ]
           ]
 
         div
           [
-            Classes
-              [
-                tailwind.``text-green-600``
-                tailwind.``text-center``
-                tailwind.``w-full``
-              ]
+            Classes [
+              tailwind.``text-green-600``
+              tailwind.``text-center``
+              tailwind.``w-full``
+            ]
           ]
           [
             str (sprintf "%A" (generateValueByForm typeof<UserProfile> UserProfile.defaultValue state.UserProfileForm))
@@ -270,12 +283,11 @@ let app state dispatch =
 
         div
           [
-            Classes
-              [
-                tailwind.``text-gray-500``
-                tailwind.``text-center``
-                tailwind.``w-full``
-              ]
+            Classes [
+              tailwind.``text-gray-500``
+              tailwind.``text-center``
+              tailwind.``w-full``
+            ]
           ]
           [
             str (sprintf "%A" UserProfile.defaultValue)
--- src/Fun.LightForm.Fable/FormViews.fs
+++ src/Fun.LightForm.Fable/FormViews.fs
@@ -8,158 +8,182 @@ open Fable.React.Props
 type FieldRenderer = FieldRenderer<Fable.React.ReactElement>
 
 
-type InputType =
+let field (state: LightForm) dispatch key (render: FieldRenderer) =
+  state
+  |> List.tryFind (fun x -> x.Name = key)
+  |> Option.map (fun field ->
+      fragment 
+          [ FragmentProp.Key field.Name ] 
+          [ render field dispatch ])
+  |> Option.defaultValue 
+      (div [ Style [ Color "red" ] ] 
+           [ str (sprintf "Field is not configured for %s" key) ])
+
+
+let fieldOuter children classes =
+  div
+    [
+      match classes with
+      | Some cs -> yield Class cs
+      | None    -> yield Style [ Margin "5px"; Padding "5px" ]
+    ]
+    children
+
+
+let fieldLabel label classes =
+  div
+    [
+      match classes with
+      | Some cs -> yield Class cs
+      | None    -> yield Style [ Color "gray"; FontSize "0.9rem" ]
+    ]
+    [ str label ]
+
+
+let fieldError fieldValue classes =
+    match fieldValue with
+    | Valid _ -> []
+    | Invalid (_, es) ->
+         [
+            for e in es ->
+              div
+                [
+                  match classes with
+                  | Some cs -> yield Class cs
+                  | None    -> yield Style [ Color "red"; FontSize "0.8rem"; Opacity "0.8" ]
+                ]
+                [ str e ]
+         ]
+
+let reduceClasses cs = cs |> String.concat " "
+let inline private foldClasses defaultValue props = props |> UnionCase.caseValues defaultValue |> UnionCase.toOptions |> Option.map reduceClasses
+
+type InputProp =
+  | InputType of InputType
+  | Label of string
+  | OuterClasses of string
+  | InputClasses of string
+  | LabelClasses of string
+  | ErrorClasses of string
+  | InputViewWrapper of (ReactElement -> ReactElement)
+and InputType =
   | Email
   | Text
   | Number
   | Password
   | Date of format: string
 
+let inputField (props: InputProp list): FieldRenderer =
+    fun field dispatch ->
 
-type Fields() =
-  static member field (state: LightForm) dispatch key (render: FieldRenderer) =
-    state
-    |> List.tryFind (fun x -> x.Name = key)
-    |> Option.map (fun field ->
-        fragment 
-            [ FragmentProp.Key field.Name ] 
-            [ render field dispatch ])
-    |> Option.defaultValue 
-        (div [ Style [ Color "red" ] ] 
-             [ str (sprintf "Field is not configured for %s" key) ])
+      let label             = props |> UnionCase.caseValues (InputProp.Label "") |> UnionCase.toOption
+      let inputType         = props |> UnionCase.caseValues (InputProp.InputType InputType.Text) |> UnionCase.value
+      let inputViewWrapper  = props |> UnionCase.caseValues (InputProp.InputViewWrapper id) |> UnionCase.toOption
+      let outerClasses      = props |> foldClasses (InputProp.OuterClasses "")
+      let labelClasses      = props |> foldClasses (InputProp.LabelClasses "")
+      let inputClasses      = props |> foldClasses (InputProp.InputClasses "")
+      let errorClasses      = props |> foldClasses (InputProp.ErrorClasses "")
 
-  static member fieldOuter(children, classes) =
-    div
-      [
-        match classes with
-        | Some cs -> yield Class cs
-        | None    -> yield Style [ Margin "5px"; Padding "5px" ]
-      ]
-      children
-
-  static member fieldLabel(label, classes) =
-    div
-      [
-        match classes with
-        | Some cs -> yield Class cs
-        | None    -> yield Style [ Color "gray"; FontSize "0.9rem" ]
-      ]
-      [ str label ]
-
-  static member fieldError(fieldValue, classes) =
-      match fieldValue with
-      | Valid _ -> []
-      | Invalid (_, es) ->
-           [
-              for e in es ->
-                div
-                  [
-                    match classes with
-                    | Some cs -> yield Class cs
-                    | None    -> yield Style [ Color "red"; FontSize "0.8rem"; Opacity "0.8" ]
-                  ]
-                  [ str e ]
-           ]
-
-  static member input
-    (inputType: InputType
-    ,?label: string
-    ,?outerClasses: string
-    ,?labelClasses: string
-    ,?errorClasses: string
-    ,?inputClasses: string
-    ,?inputViewWrapper: ReactElement -> ReactElement)
-    : FieldRenderer =
-      fun field dispatch ->
-        let inputView = 
-          input
-            [
-              match inputClasses with
-              | Some cs -> yield Class cs
+      let inputView = 
+        input
+          [
+            match inputClasses with
               | None    -> yield Style [ Width "100%"; Margin "2px 0"; Padding "2px 5px"; BackgroundColor "#f1f1f1" ]
-              yield Type (
-                    match inputType with
-                    | Email -> "email"
-                    | Date _ -> "date"
-                    | Text -> "text"
-                    | Number -> "number"
-                    | Password -> "password")
-              yield DefaultValue (
-                  let value = field |> getFormFieldValue
+              | Some cs -> yield Class cs
+            yield Type (
                   match inputType with
-                  | Email
-                  | Text
-                  | Password
-                  | Number -> if isNull value then box "" else box value
-                  | Date f ->
-                      try (value :?> DateTime).ToString(f) |> box
-                      with _ -> box value)
-              yield OnChange (fun e ->
-                  match inputType with
-                  | Email
-                  | Text
-                  | Password
-                  | Number
-                  | Date _ -> ChangeField (field.Name, e.Value)
-                  |> dispatch)
-            ]
-        Fields.fieldOuter
-          ([
-            if label.IsSome then
-              yield Fields.fieldLabel(label.Value, labelClasses) 
-
-            match inputViewWrapper with
-            | Some wrapper -> yield wrapper inputView
-            | None         -> yield inputView
-
-            yield! Fields.fieldError (field.Value, errorClasses)
+                  | Email -> "email"
+                  | Date _ -> "date"
+                  | Text -> "text"
+                  | Number -> "number"
+                  | Password -> "password")
+            yield DefaultValue (
+                let value = field |> getFormFieldValue
+                match inputType with
+                | Email
+                | Text
+                | Password
+                | Number -> if isNull value then box "" else box value
+                | Date f ->
+                    try (value :?> DateTime).ToString(f) |> box
+                    with _ -> box value)
+            yield OnChange (fun e ->
+                match inputType with
+                | Email
+                | Text
+                | Password
+                | Number
+                | Date _ -> ChangeField (field.Name, e.Value)
+                |> dispatch)
           ]
-         ,outerClasses)
+      fieldOuter
+        [
+          if label.IsSome then
+            yield fieldLabel label.Value labelClasses
 
-  static member selector<'id, 'v when 'id : equality>
-    (sourceList: ('id * 'v) list
-    ,?displayer: ('id * 'v -> ReactElement)
-    ,?label: string
-    ,?outerClasses: string
-    ,?labelClasses: string
-    ,?errorClasses: string
-    ,?inputClasses: string)
-    : FieldRenderer =
-      fun field disp ->
-        let ids =
-          try field |> getFormFieldValue :?> 'id seq
-          with _ -> [||] :> 'id seq
-        let createNewValue id =
-          ids
-          |> List.ofSeq
-          |> List.partition (fun id' -> id = id')
-          |> function
-            | [], x -> id::x
-            | _, x  -> x
-          |> List.toArray
-        Fields.fieldOuter
-          ([
-            if label.IsSome then
-              yield Fields.fieldLabel(label.Value, labelClasses) 
+          match inputViewWrapper with
+          | Some wrapper -> yield wrapper inputView
+          | None         -> yield inputView
 
-            for (id, v) in sourceList do
-              yield div
-                [
-                  Class (inputClasses |> Option.defaultValue "")
-                ]
-                [
-                  yield input
-                    [
-                      Type "checkbox"
-                      Value (ids |> Seq.exists ((=) id))
-                      OnChange (fun _ -> ChangeField (field.Name, box (createNewValue id)) |> disp)
-                    ]
+          yield! fieldError field.Value errorClasses
+        ]
+       outerClasses
 
-                  match displayer with
-                  | Some dis -> yield dis (id, v)
-                  | None     -> yield v |> box |> string |> str
-                ]
 
-            yield! Fields.fieldError(field.Value, errorClasses) 
-          ]
-         ,outerClasses)
+
+type SelectorProp<'id, 'v> =
+  | SourceList of ('id * 'v) list
+  | Displayer of ('id * 'v -> ReactElement)
+  | Label of string
+  | OuterClasses of string
+  | LabelClasses of string
+  | ErrorClasses of string
+  | InputClasses of string
+
+let inline selectorField<'id, 'v when 'id : equality> (props: SelectorProp<'id, 'v> list): FieldRenderer =
+    fun field disp ->
+      let ids =
+        try field |> getFormFieldValue :?> 'id seq
+        with _ -> [||] :> 'id seq
+
+      let createNewValue id =
+        ids
+        |> List.ofSeq
+        |> List.partition (fun id' -> id = id')
+        |> function
+          | [], x -> id::x
+          | _, x  -> x
+        |> List.toArray
+
+      let label         = props |> UnionCase.caseValues (SelectorProp.Label "") |> UnionCase.toOption
+      let sourceList    = props |> UnionCase.caseValues (SelectorProp.SourceList []) |> UnionCase.value
+      let displayer     = props |> UnionCase.caseValues (SelectorProp.Displayer (fun (_, v) -> v.ToString() |> str)) |> UnionCase.value
+      let outerClasses  = props |> foldClasses (SelectorProp.OuterClasses "")
+      let labelClasses  = props |> foldClasses (SelectorProp.LabelClasses "")
+      let inputClasses  = props |> foldClasses (SelectorProp.InputClasses "")
+      let errorClasses  = props |> foldClasses (SelectorProp.ErrorClasses "")
+
+      fieldOuter
+        [
+          if label.IsSome then
+            yield fieldLabel label.Value labelClasses
+
+          for (id, v) in sourceList do
+            yield div
+              [
+                Class (inputClasses |> Option.defaultValue "")
+              ]
+              [
+                yield input
+                  [
+                    Type "checkbox"
+                    Value (ids |> Seq.exists ((=) id))
+                    OnChange (fun _ -> ChangeField (field.Name, box (createNewValue id)) |> disp)
+                  ]
+
+                yield displayer (id, v)
+              ]
+
+          yield! fieldError field.Value errorClasses
+        ]
+        outerClasses
